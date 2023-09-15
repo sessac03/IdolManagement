01f29e61d41c25e9a304a3918511982b93fb7894
wbham <wbham97@naver.com>
2023-09-15 14:02:08 +0900

    [Add] - main initialsetting

--- AgentManagement/src/main/kotlin/com/agent/manage/AgentManage.kt
+++ AgentManagement/src/main/kotlin/com/agent/manage/AgentManage.kt
@@ -1,5 +1,20 @@
 package com.agent.manage
 
+import java.io.File
+
+fun startManage(){
+    val fileOut = File("./managementfile/manage.dat").bufferedWriter(Charsets.UTF_8)
+    println("++++++++++++++++++++++++++++++++++++++++++++++++")
+    println("메뉴:    1. 회사 관리    2. 아이돌 관리   4. 행사관리")
+    println("++++++++++++++++++++++++++++++++++++++++++++++++")
+
+    var line: String?
+    line = ConsoleReader.consoleLineInput()
+    while (!line.isNotEmpty() && !line.equals("exit", true)){
+
+    }
+}
+
 fun main(){
-    
+    startManage()
 }
